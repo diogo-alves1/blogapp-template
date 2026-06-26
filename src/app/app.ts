7a48bf4b063aad22ba5b3713cba5fc8f3e8e64ae
Diogo Alves <diogo.alves@hftm.ch>
2026-06-26 18:48:22 +0200

    feat: eigene Farbpalette definieren und dark mode

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-
+import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatToolbarModule, MatButtonModule],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = 'HFTM Web Applications (IN353)';
+
+  isDarkMode = false;
+
+  toggleTheme(): void {
+    this.isDarkMode = !this.isDarkMode;
+    document.body.classList.toggle('dark-theme', this.isDarkMode);
+  }
 }
--- src/app/feature/blog-detail-page/blog-detail-page.ts
+++ src/app/feature/blog-detail-page/blog-detail-page.ts
@@ -1,12 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { Blog } from '../../core/utils/blog-model';
 
 @Component({
   selector: 'app-blog-detail-page',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './blog-detail-page.html',
   styleUrl: './blog-detail-page.scss',
 })
@@ -14,4 +15,14 @@ export class BlogDetailPage {
   private route = inject(ActivatedRoute);
 
   blog = this.route.snapshot.data['blog'] as Blog;
+
+  toggleLike(): void {
+    this.blog.likedByMe = !this.blog.likedByMe;
+
+    if (this.blog.likedByMe) {
+      this.blog.likes++;
+    } else {
+      this.blog.likes--;
+    }
+  }
 }
