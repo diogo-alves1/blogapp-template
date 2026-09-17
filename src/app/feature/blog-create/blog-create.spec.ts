7c75f857e801e9440a218f8e4e48f2e528f4b9cb
Diogo Alves <diogo.alves@hftm.ch>
2026-09-17 19:13:32 +0200

    test: configure router for blog create component

--- src/app/feature/blog-create/blog-create.spec.ts
+++ src/app/feature/blog-create/blog-create.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 
 import { BlogCreate } from './blog-create';
 
@@ -9,6 +10,7 @@ describe('BlogCreate', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [BlogCreate],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(BlogCreate);
