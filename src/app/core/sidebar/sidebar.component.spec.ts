6516d427077918c42bd95b7e308559e35ffd702f
Diogo Alves <diogo.alves@hftm.ch>
2026-09-24 17:29:51 +0200

    test: update app toolbar test

--- src/app/core/sidebar/sidebar.component.spec.ts
+++ src/app/core/sidebar/sidebar.component.spec.ts
@@ -1,7 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
-import { provideBreakpointObserver } from '@angular/cdk/layout';
 
 import { SidebarComponent } from './sidebar.component';
 
@@ -12,7 +11,7 @@ describe('SidebarComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [SidebarComponent],
-      providers: [provideRouter([]), provideNoopAnimations(), provideBreakpointObserver()],
+      providers: [provideRouter([]), provideNoopAnimations()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SidebarComponent);
